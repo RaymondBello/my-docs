60f876c8b7abf4c8bd844f982b8118d6639bb23b
Ray <61327086+RaymondBello@users.noreply.github.com>
2024-03-06 14:27:14 -0500

    Revert "Updated website design"
    This reverts commit 9065fc24ef73b816bf78612f149be1f23282ae24.

--- my-website/src/components/HomepageExp.js
+++ my-website/src/components/HomepageExp.js
@@ -1,76 +1,99 @@
-import React from "react";
-import clsx from "clsx";
-import styles from "./HomepageExp.module.css";
+import React from 'react';
+import clsx from 'clsx';
+import styles from './HomepageExp.module.css';
 
 const ExperienceList = [
-  {
-    title: "HBM Digital Design Sr Engineer",
-    Svg: require("../../static/img/synopsys.svg").default,
-    description: <>Jan 2024 - Present</>,
-  },
-  {
-    title: "SERDES Digital Design Engineer I/II",
-    Svg: require("../../static/img/synopsys.svg").default,
-    description: <>May 2022 - Dec 2023</>,
-  },
-  {
-    title: "HBM Verification Engineer Intern",
-    Svg: require("../../static/img/synopsys.svg").default,
-    description: <>Jan 2021 - May 2022</>,
-  },
-  {
-    title: "ASIC Digital Verification Engineer CO-OP",
-    Svg: require("../../static/img/ciena.svg").default,
-    description: <>Jan 2020 - July 2020</>,
-  },
-  {
-    title: "Printing Services Technician",
-    Svg: require("../../static/img/TPH.svg").default,
-    description: <>Summer 2019</>,
-  },
-  {
-    title: "Technical Sales Associate",
-    Svg: require("../../static/img/incredible-connection.svg").default,
-    description: <>Summer 2017 & 2018</>,
-  },
-  {
-    title: "BASc Electrical Engineering and Computer Technology",
-    Svg: require("../../static/img/uottawa.svg").default,
-    description: <>2015 - 2021</>,
-  },
-  {
-    title: "Ontario Secondary School Diploma",
-    Svg: require("../../static/img/soc.svg").default,
-    description: <>2015</>,
-  },
+    {
+      title: 'SERDES Digital Design Engineer',
+      Svg: require('../../static/img/synopsys.svg').default,
+      description: (
+        <>
+          May 2022 - Present
+        </>
+      ),
+    },
+    {
+      title: 'HBM Verification Engineer',
+      Svg: require('../../static/img/synopsys.svg').default,
+      description: (
+        <>
+          Jan 2021 - May 2022
+        </>
+      ),
+    },
+    {
+      title: 'ASIC Digital Verification Engineer CO-OP',
+      Svg: require('../../static/img/ciena.svg').default,
+      description: (
+        <>
+          Jan 2020 - July 2020
+        </>
+      ),
+    },
+    {
+      title: 'Printing Services Technician',
+      Svg: require('../../static/img/TPH.svg').default,
+      description: (
+        <>
+          Summer 2019
+        </>
+      ),
+    },
+    {
+      title: 'Technical Sales Associate',
+      Svg: require('../../static/img/incredible-connection.svg').default,
+      description: (
+        <>
+          Summer 2017 & 2018
+        </>
+      ),
+    },
+    {
+      title: 'BASc Electrical Engineering and Computer Technology',
+      Svg: require('../../static/img/uottawa.svg').default,
+      description: (
+        <>
+          2015 - 2021
+        </>
+      ),
+    },
+    {
+      title: 'Ontario Secondary School Diploma',
+      Svg: require('../../static/img/soc.svg').default,
+      description: (
+        <>
+          2015
+        </>
+      ),
+    },
 ];
-
+  
 function Experience({ Svg, title, description }) {
-  return (
-    <div className={clsx("col col--4")}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg2} alt={title} />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className={clsx('col col--4')}>
+            <div className="text--center">
+                <Svg className={styles.featureSvg2} alt={title} />
+            </div>
+            <div className="text--center padding-horiz--md">
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
 }
 
 export default function HomepageExp() {
-  return (
-    <section className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <br />
-        <h1 className="text--center">Experience & Education</h1>
-        <div className="row">
-          {ExperienceList.map((props, idx) => (
-            <Experience key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={clsx("hero hero--primary", styles.heroBanner)}>
+            <div className="container">
+                <br/>
+                <h1 className="text--center">Experience & Education</h1>
+                <div className="row">
+                    {ExperienceList.map((props, idx) => (
+                        <Experience key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
--- my-website/src/components/HomepageProjects.js
+++ my-website/src/components/HomepageProjects.js
@@ -7,15 +7,14 @@ import useBaseUrl from "@docusaurus/useBaseUrl";
 
 const ProjectList = [
   {
-    title: "UAV Flight Controller",
+    title: "HermesFC",
     Image: require("@site/static/img/hermes.gif").default,
     link: "/docs/Hermes-rev1/overview",
     description: (
       <>
-        This 4-layer UAV flight controller stabilizes both quad-copter and
-        fixed-wing vehicles, ensuring a smooth, secure flying experience in
-        challenging terrains and maintaining a steady altitude. It's a crucial
-        component of any well-rounded UAV.
+        This 4-layer UAV flight controller is a specialized design made to stabilize both quadcopter and fixed-wing vehicles. 
+        With its active stabilization abilities, this controller ensures a smooth, secure flying experience. 
+        From navigating challenging terrains to maintaining a steady altitude, this controller is a crucial component of any well-rounded UAV.
       </>
     ),
   },
@@ -25,87 +24,20 @@ const ProjectList = [
     link: "https://github.com/RaymondBello/Telemetry-UI",
     description: (
       <>
-        This Ground Station GUI provides real-time oversight of autonomous
-        vehicles via a secure WebSocket connection. It enables remote monitoring
-        of speed, location, and operational status, allowing operators to make
-        adjustments as needed.
+        This Ground Station GUI offers a wireless means of overseeing the behavior of autonomous vehicles through a secure WebSocket connection. 
+        This interface allows remote monitoring of various data points, including speed, location, and operational status, enabling operators to remotely make changes and adjustments when necessary. 
+        By providing real-time visibility into autonomous vehicle operations, the Ground Station UI offers enhanced control, convenience, and overall safety.
       </>
     ),
   },
   {
-    title: "Aircraft Physics Simulation",
-    Image: require("@site/static/img/TakeOff_Demo.gif").default,
-    link: "https://github.com/RaymondBello/Glider-Autopilot/tree/main/Flight%20Simulation/Unity%20Aircraft%20Simulation",
-    description: (
-      <>
-        This a fixed-wing physics simulation written in Unity. The goal of the
-        project is to create a physics simulation environment to test and tune
-        fixed-wing flight controller gain parameters.
-      </>
-    ),
-  },
-  {
-    title: "Flight Planner Website",
-    Image: require("@site/static/img/mission_planner.png").default,
-    link: "https://github.com/RaymondBello/Glider-Autopilot/tree/main/Ground%20Control%20Software/Python%20Telemetry%20Dashboard/web",
-    description: (
-      <>
-        I created a user-friendly website that enables the seamless organization
-        and oversight of navigation points for an unmanned fixed-wing aircraft.
-        This platform aids in the efficient planning and management of the
-        plane's flight path.
-      </>
-    ),
-  },
-  {
-    title: "A* Path Finding Algorithm",
-    Image: require("@site/static/img/a-star.gif").default,
-    link: "https://github.com/RaymondBello/A-Star-Algorithm-Visualization",
-    description: (
-      <>
-        The purpose of this application is to provide a dynamic and engaging
-        platform for users to explore the A* Pathfinding algorithm's
-        functionality. With Python as its programming language, users can easily
-        interact with and manipulate the algorithm to best suit their needs.
-      </>
-    ),
-  },
-  {
-    title: "Web-based Flight Instruments",
-    Image: require("@site/static/img/pfd.png").default,
-    link: "https://github.com/RaymondBello/Glider-Autopilot/tree/main/Ground%20Control%20Software/Python%20Telemetry%20Dashboard/web",
-    description: (
-      <>
-        I created an online cockpit display panel for pilots with primary flight
-        instruments. Featuring advanced programming techniques using scalable
-        vector graphics (SVG) and javascript, it can synchronize telemetry data
-        to create a highly responsive and efficient display interface.
-      </>
-    ),
-  },
-  {
-    title: "Photo Archive App",
+    title: "gram",
     Image: require("@site/static/img/ray-gram.gif").default,
     link: "https://github.com/RaymondBello/ray-gram",
     description: (
       <>
-        This React-based photo library app is a convenient platform that allows
-        artists and photographers to showcase their diverse projects and fashion
-        shoots. Through a user-friendly interface, users can easily browse and
-        organize their photo collections, while leveraging various tools and
-        customizable features to highlight their creative work.
-      </>
-    ),
-  },
-  {
-    title: "Baku Autonomous Racer",
-    Image: require("@site/static/img/track.png").default,
-    link: "",
-    description: (
-      <>
-        Designed a control system for an RC car that allows the car to self
-        optimize lap times by utilizing onboard IMU sensors to maximize traction
-        while finding the shortest local path.
+       This React-based photo library app is a convenient platform that allows artists and photographers to showcase their diverse projects and fashion shoots. 
+       Through a user-friendly interface, users can easily browse and organize their photo collections, while leveraging various tools and customizable features to highlight their creative work.
       </>
     ),
   },
@@ -122,21 +54,81 @@ const ProjectList = [
       </>
     ),
   },
+  {
+    title: "A* Path Finding Algorithm Visualization",
+    Image: require("@site/static/img/a-star.gif").default,
+    link: "https://github.com/RaymondBello/A-Star-Algorithm-Visualization",
+    description: (
+      <>
+        The purpose of this application is to provide a dynamic and engaging platform for users to explore the A* Pathfinding algorithm's functionality. 
+      With Python as its programming language, users can easily interact with and manipulate the algorithm to best suit their needs.
+      </>
+    ),
+  },
+  {
+    title: "Flight Planner Website",
+    Image: require("@site/static/img/mission_planner.png").default,
+    link: "https://github.com/RaymondBello/Glider-Autopilot/tree/main/Ground%20Control%20Software/Python%20Telemetry%20Dashboard/web",
+    description: (
+      <>
+        I created a user-friendly website that enables the seamless organization and oversight of navigation points for an unmanned fixed-wing aircraft.
+      This platform aids in the efficient planning and management of the plane's flight path.
+      </>
+    ),
+  },
+  {
+    title: "Aircraft Physics Simulation",
+    Image: require("@site/static/img/TakeOff_Demo.gif").default,
+    link: "https://github.com/RaymondBello/Glider-Autopilot/tree/main/Flight%20Simulation/Unity%20Aircraft%20Simulation",
+    description: (
+      <>
+        This a fixed-wing physics simulation written in Unity. The goal of the
+        project is to create a physics simulation environment to test and tune
+        fixed-wing flight controller gain parameters.
+      </>
+    ),
+  },
+  {
+    title: "Baku Autonomous Racer",
+    Image: require("@site/static/img/track.png").default,
+    link: "",
+    description: (
+      <>
+        Designed a control system for an RC car that allows the car to self
+        optimize lap times by utilizing onboard IMU sensors to maximize traction
+        while finding the shortest local path.
+      </>
+    ),
+  },
+  {
+    title: "Web-based Flight Instruments",
+    Image: require("@site/static/img/pfd.png").default,
+    link: "https://github.com/RaymondBello/Glider-Autopilot/tree/main/Ground%20Control%20Software/Python%20Telemetry%20Dashboard/web",
+    description: (
+      <>
+        I created an online cockpit display panel for pilots with primary flight instruments. 
+        Featuring advanced programming techniques using scalable vector graphics (SVG) and javascript, it can synchronize telemetry data to create a highly responsive and efficient display interface.
+      </>
+    ),
+  },
 ];
 
 function Project({ Image, title, description, link }) {
   return (
-    <div className="cards" >
-      <div className="card">
-        <img src={Image} alt={title} />
-        <div className="card-content">
-          <h2>{title}</h2>
-          <p>{description}</p>
-          <Link className="button button--secondary button--sm" to={link}>
-            see more...
-          </Link>
-        </div>
+    <div className={clsx("col col--4")}>
+      <div className="text--center">
+        <img src={Image} alt={title} className={styles.featureImg} />
+        {/* <img src={Image} alt={title}  /> */}
       </div>
+
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <Link className="button button--secondary button--lg" to={link}>
+          see more...
+        </Link>
+      </div>
+      <br />
     </div>
   );
 }
@@ -147,12 +139,13 @@ export default function HomepageProjects() {
       <div className="container">
         <br />
         <h1 className="text--center">Projects</h1>
+        <br /> 
+
         <div className="row">
           {ProjectList.map((props, idx) => (
             <Project key={idx} {...props} />
           ))}
         </div>
-        <br />
       </div>
     </section>
   );
